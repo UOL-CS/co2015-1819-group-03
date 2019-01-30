19757dad19dc6e866d4616a125893304f3d714c5
hoshildharmendra <hd180@mcs.le.ac.uk>
2019-01-30 14:36:05 +0000

    Setup skeleton spring boot application

---
+++ src/main/java/group3/gitruler/GitrulerApplication.java
@@ -0,0 +1,14 @@
+package group3.gitruler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitrulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitrulerApplication.class, args);
+	}
+
+}
+

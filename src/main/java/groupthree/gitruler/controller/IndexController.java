a446c56370731671fe6eb3df49db0cf1e9ad8f50
Hoshil Dharmendra <hd180@student.le.ac.uk>
2019-02-04 15:22:56 +0000

    Add route for landing and exercises page

---
+++ src/main/java/groupthree/gitruler/controller/IndexController.java
@@ -0,0 +1,21 @@
+package groupthree.gitruler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
+
+	@RequestMapping("/exercises")
+	public String exerciseList(Model model) {
+		return "exercises";
+	}
+
+}

5b5312806cb51f197f0dc356047167cb88bb1258
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-02-05 20:02:28 +0000

    Redirect to exercises page from index page if authenticated

--- src/main/java/groupthree/gitruler/controller/IndexController.java
+++ src/main/java/groupthree/gitruler/controller/IndexController.java
@@ -1,5 +1,8 @@
 package groupthree.gitruler.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
 
+  /**
+   * Handles the route for the index page. Redirect to exercises page if
+   * authenticated.
+   */
   @RequestMapping("/")
   public String index(Model model) {
+
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+    if (!(auth instanceof AnonymousAuthenticationToken)) {
+      return "redirect:/exercises";
+    }
+
     return "index";
   }
 
+  /** Handles the route for the exercises page containing the list of exercises. */
   @RequestMapping("/exercises")
   public String exerciseList(Model model) {
     return "exercises";

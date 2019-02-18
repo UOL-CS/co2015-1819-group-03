0def7d8c2f07cbe0e441b7e38175b8d5bc193f66
newk1 <emmanuel@warna98.eu>
2019-02-18 22:12:37 +0000

    Allow unauthenticated access to css folder

--- src/main/java/groupthree/gitruler/SecurityConfig.java
+++ src/main/java/groupthree/gitruler/SecurityConfig.java
@@ -10,13 +10,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .authorizeRequests()
-        .anyRequest()
-        .authenticated()
-      .and()
-        .oauth2Login()
-          .loginPage("/")
-          .defaultSuccessUrl("/exercises")
-          .permitAll();
+        .authorizeRequests()
+          .antMatchers("/css/**")
+          .permitAll()
+        .and()
+          .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+        .and()
+          .oauth2Login()
+            .loginPage("/")
+            .defaultSuccessUrl("/exercises")
+            .permitAll();
   }
 }

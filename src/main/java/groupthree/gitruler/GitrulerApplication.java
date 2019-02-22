31995954a9bc7e398574541a646106ae97093888
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-02-22 19:11:29 +0000

    Save dummy exercise data into repository

--- src/main/java/groupthree/gitruler/GitrulerApplication.java
+++ src/main/java/groupthree/gitruler/GitrulerApplication.java
@@ -1,7 +1,13 @@
 package groupthree.gitruler;
 
+import groupthree.gitruler.domain.Exercise;
+import groupthree.gitruler.repository.ExerciseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 /**
  * This class contains the main method that is called by Spring to run the
@@ -9,9 +15,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class GitrulerApplication {
+  
+  @Autowired
+  private ExerciseRepository exRepo;
 
   public static void main(String[] args) {
     SpringApplication.run(GitrulerApplication.class, args);
+  }
+  
+  /**
+   * This method catches the on ApplicationReadyEvent and inserts dummy data
+   * into the exercise repository when caught.
+   */
+  @EventListener
+  public void appReady(ApplicationReadyEvent event) {
+
+    Exercise ex;
+    
+    for (int i = 0; i <= 10; i++) {
+      ex = new Exercise();
+      ex.setName("Exercise " + i);
+      ex.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+          + "Aenean mattis vel purus et sagittis nullam.");
+      exRepo.save(ex);
+    }
+      
   }
 
 }

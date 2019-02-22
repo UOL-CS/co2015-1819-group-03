3a7e3d8981a1d0d04082979cc1b620e9eb72699d
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-02-22 19:12:04 +0000

    Pass retrieved exercises to model

--- src/main/java/groupthree/gitruler/controller/IndexController.java
+++ src/main/java/groupthree/gitruler/controller/IndexController.java
@@ -1,14 +1,24 @@
 package groupthree.gitruler.controller;
 
+import groupthree.gitruler.domain.Exercise;
+import groupthree.gitruler.repository.ExerciseRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+  
+  @Autowired
+  private ExerciseRepository exRepo;
 
   /**
    * Handles the route for the index page. Redirect to exercises page if
@@ -30,7 +40,17 @@ public class IndexController {
    * Handles the route for the exercises page containing the list of exercises.
    */
   @RequestMapping("/exercises")
-  public String exerciseList() {
+  public String exerciseList(Model model) {
+    
+    List<Exercise> exercises;
+    exercises = (List<Exercise>) exRepo.findAll();
+
+    if (exercises.isEmpty()) {
+      System.out.println("empty db");
+    } else {
+      model.addAttribute("exercises", exercises);
+    }
+    
     return "exercises";
   }
 

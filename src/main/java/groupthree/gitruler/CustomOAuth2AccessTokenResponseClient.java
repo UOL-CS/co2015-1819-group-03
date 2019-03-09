42b003b3eaa1288026ceee7901fbb320469286ae
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-03-09 21:58:54 +0000

    Add custom tokenEndpoint to intercept access token and authenticate user

---
+++ src/main/java/groupthree/gitruler/CustomOAuth2AccessTokenResponseClient.java
@@ -0,0 +1,77 @@
+package groupthree.gitruler;
+
+import java.util.Collections;
+import java.util.Set;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AccessTokenResponse;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestOperations;
+
+public class CustomOAuth2AccessTokenResponseClient
+    implements OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> {
+
+  private RestOperations restOperations;
+
+  public CustomOAuth2AccessTokenResponseClient(RestOperations restOperations) {
+    this.restOperations = restOperations;
+  }
+  
+  @Override
+  public OAuth2AccessTokenResponse getTokenResponse(OAuth2AuthorizationCodeGrantRequest
+      authorizationGrantRequest) throws OAuth2AuthenticationException {  
+    
+    ClientRegistration clientRegistration = authorizationGrantRequest.getClientRegistration();
+    String tokenUri = clientRegistration.getProviderDetails().getTokenUri();
+    
+    String clientId = clientRegistration.getClientId();
+    String clientSecret = clientRegistration.getClientSecret();
+    String grantType = clientRegistration.getAuthorizationGrantType().getValue();
+    String code = authorizationGrantRequest
+                        .getAuthorizationExchange().getAuthorizationResponse().getCode();
+    String uri = authorizationGrantRequest
+                        .getAuthorizationExchange().getAuthorizationRequest().getRedirectUri();
+    Set<String> scope = authorizationGrantRequest.getClientRegistration().getScopes();
+
+    MultiValueMap<String, String> tokenRequest = new LinkedMultiValueMap<String, String>();
+    tokenRequest.add("client_id", clientId);
+    tokenRequest.add("client_secret", clientSecret);
+    tokenRequest.add("grant_type", grantType);
+    tokenRequest.add("code", code);
+    tokenRequest.add("redirect_uri", uri);
+    tokenRequest.add("scope", String.join(" ", scope));
+    
+    HttpHeaders headers = new HttpHeaders();
+    HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(tokenRequest, headers);
+
+    ResponseEntity<AccessResponseBody> response =
+        restOperations.exchange(tokenUri, HttpMethod.POST, entity, AccessResponseBody.class);
+        
+    AccessResponseBody accessResponseBody = new AccessResponseBody();
+    Set<String> scopes = Collections.emptySet();
+    
+    if (response != null) {
+      accessResponseBody = response.getBody();
+      
+      if (accessResponseBody.getScopes().isEmpty()) {
+        scopes = scope;
+      } else {
+        scopes = accessResponseBody.getScopes();
+      }
+    }
+    
+    return OAuth2AccessTokenResponse.withToken(accessResponseBody.getAccessToken())
+        .tokenType(accessResponseBody.getTokenType())
+        .expiresIn(accessResponseBody.getExpiresIn())
+        .scopes(scopes)
+        .build();
+  }
+
+}
--- src/main/java/groupthree/gitruler/SecurityConfig.java
+++ src/main/java/groupthree/gitruler/SecurityConfig.java
@@ -1,13 +1,21 @@
 package groupthree.gitruler;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @EnableWebSecurity
 @EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  @Bean
+  public RestOperations restOperations() {
+    return new RestTemplate();
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -23,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .oauth2Login()
             .loginPage("/")
             .defaultSuccessUrl("/exercises")
-            .permitAll();
+            .permitAll()
+            .tokenEndpoint()
+            .accessTokenResponseClient(new CustomOAuth2AccessTokenResponseClient(restOperations()));
   }
 }

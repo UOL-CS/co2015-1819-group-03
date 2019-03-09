7f98346031e70bae4edb88b62f1521d3aba319e7
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-03-09 18:50:23 +0000

    Change import order to be sorted lexographically

--- src/main/java/groupthree/gitruler/repository/UserRepository.java
+++ src/main/java/groupthree/gitruler/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package groupthree.gitruler.repository;
 
+import groupthree.gitruler.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-import groupthree.gitruler.domain.User;
-
-public interface UserRepository extends CrudRepository <User,Integer>{
+public interface UserRepository extends CrudRepository<User,Integer> {
   User findById(int id);
 }

86b230ae92fa49081e926d353ff8156298d17faf
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-03-09 18:55:54 +0000

    Add oauth2 dependency for @EnableOAuth2Client and annotate security config

--- src/main/java/groupthree/gitruler/SecurityConfig.java
+++ src/main/java/groupthree/gitruler/SecurityConfig.java
@@ -3,8 +3,10 @@ package groupthree.gitruler;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @EnableWebSecurity
+@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override

9e5d91af736f76df596029f5283aacca9dfd2f55
hoshildharmendra <hoshil.dharmendra@live.co.uk>
2019-03-10 11:01:07 +0000

    Pass user repository to class to access autowired repository within class

--- src/main/java/groupthree/gitruler/security/SecurityConfig.java
+++ src/main/java/groupthree/gitruler/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package groupthree.gitruler.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,9 +9,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
+import groupthree.gitruler.repository.UserRepository;
+
 @EnableWebSecurity
 @EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  @Autowired
+  private UserRepository userRepo;
   
   @Bean
   public RestOperations restOperations() {
@@ -35,6 +41,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .tokenEndpoint()
             .accessTokenResponseClient(new CustomOAuth2AccessTokenResponseClient(restOperations()))
             .and()
-            .userInfoEndpoint().userService(new CustomOAuth2UserService(restOperations()));
+            .userInfoEndpoint().userService(new CustomOAuth2UserService(restOperations(), userRepo));
   }
 }

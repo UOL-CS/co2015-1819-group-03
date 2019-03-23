f4bd49f00940f5a6f6a675b2a96902fb4d520569
newk1 <emmanuel@warna98.eu>
2019-03-23 22:38:11 +0000

    Add job queue class and repository

---
+++ src/main/java/groupthree/gitruler/domain/JobQueue.java
@@ -0,0 +1,57 @@
+package groupthree.gitruler.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "queue")
+public class JobQueue {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.TABLE)
+  @Column(name = "id", unique = true, nullable = false)
+  private int id;
+
+  @Column(name = "link")
+  private String link;
+
+  @Column(name = "userid")
+  private int userId;
+
+  @Column(name = "exerciseid")
+  private int exerciseId;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public int getExerciseId() {
+    return exerciseId;
+  }
+
+  public void setExerciseId(int exerciseId) {
+    this.exerciseId = exerciseId;
+  }
+}

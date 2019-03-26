49b97c6e56e3ac05e4821edddfd6c52fe41b215a
Hoshil Dharmendra <hd180@student.le.ac.uk>
2019-03-26 21:34:21 +0000

    Add useful server status messages

--- server/gitruler-server.sh
+++ server/gitruler-server.sh
@@ -13,21 +13,41 @@ fi
 cloneDir=mark
 mysqlcommand="mysql --defaults-file=$1 -e"
 
+echo "[Server starting]"
+
 while true; do
     while [[ $($mysqlcommand "SELECT COUNT(*) FROM queue;" -sN) -gt 0 ]]; do
         eval "$mysqlcommand 'SELECT * FROM queue;' -sN" | while read -r id exId link userId; do
-          rm -rf "$cloneDir"
-          git clone --quiet "$link" "$cloneDir"
+            echo "[Starting Job $id]"
+            rm -rf "$cloneDir"
+            if git clone --quiet "$link" "$cloneDir"; then
+                echo "[Clone successful]"
 
-          output=$(java -jar gitruler.jar -w -r $cloneDir)
+                output=$(java -jar gitruler.jar -w -r $cloneDir)
 
-          score=$(echo "$output" | tail -1)
+                score=$(echo "$output" | tail -1)
 
-          eval "$mysqlcommand 'INSERT INTO attempt VALUES (0, $score, $exId, $userId);'"
-          eval "$mysqlcommand 'DELETE FROM queue WHERE id=$id;' -sN";
+                if eval "$mysqlcommand 'INSERT INTO attempt VALUES (0, $score, $exId, $userId);'"; then
+                    echo "[Row insertion successful]"
+                else 
+                    echo "[Row insertion failed]"
+                fi
+
+            else 
+                echo "[Clone failed]"
+            fi
+
+            if eval "$mysqlcommand 'DELETE FROM queue WHERE id=$id;' -sN"; then
+                echo "[Row deletion successful]"
+            else
+                echo "[Row deletion failed]"
+            fi
+
         done
     done
+    echo "[Server sleeping]"
     sleep 60
+    echo "[Server wake]"
 done
 
 
